fb8062722a680ea3d47bbb5e7798a8eae88581d0
vadimturkov <vadim.turkov@gmail.com>
2021-01-09 00:52:26 +0300

    milestone-2: add all possible states of data fetching

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,13 +1,27 @@
 import { useState, useEffect } from 'react';
 
 export function useFetch(endpoint) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch(endpoint)
-      .then((response) => response.json())
-      .then((json) => setData(json));
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(endpoint);
+        const json = await response.json();
+        setData(json);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
   }, [endpoint]);
 
-  return data;
+  return { isLoading, error, data };
 }
