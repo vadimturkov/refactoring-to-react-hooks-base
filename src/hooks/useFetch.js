cc8af553e4ac5e55b5e341ff66c15bb7b2565ad2
vadimturkov <vadim.turkov@gmail.com>
2021-01-09 01:08:43 +0300

    milestone-2: replace useState with useReducer in useFetch hook

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,44 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useReducer } from 'react';
+
+const initialState = {
+  isLoading: false,
+  error: '',
+  data: [],
+};
+
+function apiReducer(state, action) {
+  switch (action.type) {
+    case 'FECTH_DATASET_START':
+      return { ...state, isLoading: true };
+    case 'FETCH_DATASET_ERROR':
+      return { ...state, isLoading: false, error: action.payload };
+    case 'FETCH_DATASET_SUCCESS':
+      return { ...state, isLoading: false, error: '', data: action.payload };
+    default:
+      return state;
+  }
+}
 
 export function useFetch(endpoint) {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [data, setData] = useState([]);
+  const [state, dispatch] = useReducer(apiReducer, initialState);
 
   useEffect(() => {
+    if (!endpoint) return;
+
     const fetchData = async () => {
-      setIsLoading(true);
+      dispatch({ type: 'FETCH_DATASET_START' });
 
       try {
         const response = await fetch(endpoint);
         const json = await response.json();
-        setData(json);
+        dispatch({ type: 'FETCH_DATASET_SUCCESS', payload: json });
       } catch (error) {
-        setError(error.message);
-      } finally {
-        setIsLoading(false);
+        dispatch({ type: 'FETCH_DATASET_ERROR', payload: error.message });
       }
     };
 
     fetchData();
   }, [endpoint]);
 
-  return { isLoading, error, data };
+  return state;
 }
