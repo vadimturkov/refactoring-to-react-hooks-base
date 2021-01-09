77005b74c47e5cd464e96adaab31aa480bf1931d
vadimturkov <vadim.turkov@gmail.com>
2021-01-09 23:25:17 +0300

    milestone-3: refactor DashboardShell component

--- src/features/Dashboard/DashboardShell.js
+++ src/features/Dashboard/DashboardShell.js
@@ -1,86 +1,55 @@
-import React, { Component } from "react";
-import Aside from "../../common/components/Aside";
-import ChartContainer from "./ChartContainer";
-import Layout from "../../common/components/Layout";
-import Main from "../../common/components/Main";
-import SummaryContainer from "./SummaryContainer";
-import { connect } from "react-redux";
-import { fetchDataset } from "./DashboardSlice";
+import React, { useState } from 'react';
 
-class DashboardShell extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { selectedLabel: "" };
+import Aside from '../../common/components/Aside';
+import Select from '../../common/components/Select';
+import Layout from '../../common/components/Layout';
+import Main from '../../common/components/Main';
+import ChartContainer from './ChartContainer';
+import SummaryContainer from './SummaryContainer';
 
-    this.handleSelectChange = this.handleSelectChange.bind(this);
-  }
+const DashboardShell = () => {
+  const [selectedLabel, setSelectedLabel] = useState('');
 
-  componentDidMount() {
-    this.props.fetchDataset(`${process.env.REACT_APP_BASE_URL}/totals/`);
-  }
+  // TODO: refactor in the next milestone
+  // componentDidMount() {
+  //   this.props.fetchDataset(`${process.env.REACT_APP_BASE_URL}/totals/`);
+  // }
 
-  handleSelectChange(event) {
+  const handleSelectChange = (event) => {
     const selectedLabel = event.target.selectedOptions[0].label;
-    this.props.fetchDataset(event.target.value);
-    this.setState({ selectedLabel });
-  }
+    setSelectedLabel(selectedLabel);
+    // TODO: refactor in the next milestone
+    // this.props.fetchDataset(event.target.value);
+  };
 
-  buildSelect() {
-    const optionsForSelect = [
-      { label: "Sales", value: `${process.env.REACT_APP_BASE_URL}/sales/` },
-      {
-        label: "Subscriptions",
-        value: `${process.env.REACT_APP_BASE_URL}/subscriptions/`
-      }
-    ];
+  const optionsForSelect = [
+    { label: 'Sales', value: `${process.env.REACT_APP_BASE_URL}/sales/` },
+    {
+      label: 'Subscriptions',
+      value: `${process.env.REACT_APP_BASE_URL}/subscriptions/`,
+    },
+  ];
 
-    return (
-      <>
-        <label htmlFor="select-product">Please select a chart:</label>
-        <div className="field">
-          <select id="select-product" onChange={this.handleSelectChange}>
-            <option value="">--</option>
-            {optionsForSelect.map(option => (
-              <option key={option.label} value={option.value}>
-                {option.label}
-              </option>
-            ))}
-          </select>
-          <div className="chevron-wrapper flex">
-            <svg
-              className="chevron"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-            >
-              <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-            </svg>
-          </div>
-        </div>
-      </>
-    );
-  }
-
-  render() {
-    return (
-      <Layout>
-        <Aside>
-          <h2># Polly dashboard</h2>
-          {this.buildSelect()}
-        </Aside>
-        <Main>
-          <h1>
-            Welcome, <span className="bold">learner!</span>
-          </h1>
-          <SummaryContainer />
-          <ChartContainer selectedLabel={this.state.selectedLabel} />
-        </Main>
-      </Layout>
-    );
-  }
-}
-
-const mapDispatchToProps = {
-  fetchDataset
+  return (
+    <Layout>
+      <Aside>
+        <h2># Polly dashboard</h2>
+        <Select
+          label='Please, select a chart'
+          id='select-chart'
+          options={optionsForSelect}
+          handleChange={handleSelectChange}
+        />
+      </Aside>
+      <Main>
+        <h1>
+          Welcome, <span className='bold'>learner!</span>
+        </h1>
+        <SummaryContainer />
+        <ChartContainer selectedLabel={selectedLabel} />
+      </Main>
+    </Layout>
+  );
 };
 
-export default connect(null, mapDispatchToProps)(DashboardShell);
+export default DashboardShell;
