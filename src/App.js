d7663dabbb6a18b61718c937c11c70c526236596
vadimturkov <vadim.turkov@gmail.com>
2021-01-09 22:30:35 +0300

    milestone-3: wrap the app with React's Context Provider

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-// import DashboardShell from './features/Dashboard/DashboardShell';
-import DataFetchingContainer from './features/Playground/DataFetchingContainer';
+
+import DashboardShell from './features/Dashboard/DashboardShell';
+import { sales } from './mocks';
+
+export const globalContext = React.createContext();
+const { Provider } = globalContext;
+
+const initialState = {
+  isLoading: false,
+  error: '',
+  salesTotal: 3466,
+  subscriptionsTotal: 1492,
+  data: sales,
+};
 
 const App = () => {
-  return <DataFetchingContainer />;
-  // return <DashboardShell />;
+  return (
+    <Provider value={initialState}>
+      <DashboardShell />
+    </Provider>
+  );
 };
 
 export default App;
