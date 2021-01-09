c9208dcd75c9cca9bd6dbea1aad79770ad349c1c
vadimturkov <vadim.turkov@gmail.com>
2021-01-09 23:26:38 +0300

    milestone-3: update Select component

--- src/common/components/Select.js
+++ src/common/components/Select.js
@@ -5,14 +5,21 @@ const Select = ({ handleChange, id, label, options }) => {
   return (
     <>
       <label htmlFor={id}>{label}</label>
-      <select id={id} onChange={handleChange}>
-        <option value=''>--</option>
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
-      </select>
+      <div className='field'>
+        <select id={id} onChange={handleChange}>
+          <option value=''>--</option>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
+        </select>
+        <div className='chevron-wrapper flex'>
+          <svg className='chevron' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 20 20'>
+            <path d='M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z' />
+          </svg>
+        </div>
+      </div>
     </>
   );
 };
